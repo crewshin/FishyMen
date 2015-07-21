d1c43fd3f817523bf58b532f389ab6cd4b746d0d
Gene Crucean <genecrucean@gmail.com>
2015-07-21 14:56:22 -0700

    Initial Commit

---
+++ FishyMen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FishyMen
+//
+//  Created by Gene Crucean on 7/21/15.
+//  Copyright (c) 2015 Dagger Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FishyMen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FishyMen
+//
+//  Created by Gene Crucean on 7/21/15.
+//  Copyright (c) 2015 Dagger Dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
